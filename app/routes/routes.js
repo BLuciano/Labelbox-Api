e19871fcfb10f06c7b7b8a6c3f858decdd95ea4c
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2022-01-26 20:28:33 -0800

    Users can now be deleted and added

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,7 +4,10 @@ const router = app => {
   });
 
   app.get("/images", (req, res) => {
-    res.send({message: "images here"});
+    pool.query('SELECT * FROM images', (error, result) => {
+        if (error) throw error;
+        response.send(result);
+    });
   });
 
   // Display all users
@@ -15,6 +18,32 @@ const router = app => {
     });
   });
 
+  // Display a single user by ID
+  app.get('/users/:id', (request, response) => {
+      const id = request.params.id;
+      pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+          if(error){throw error;}
+          response.send(result);
+      });
+  });
+
+  // Add a new user
+  app.post('/users', (request, response) => {
+    pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+        if(error){throw error;}
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+  });
+
+  // Delete a user
+  app.delete('/users/:id', (request, response) => {
+    const id = request.params.id;
+    pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+        if(error){throw error;}
+        response.send('User deleted.');
+    });
+  });
+  
   const pool = require('../data/config');
 }
 
